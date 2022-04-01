cae754a8213cca24d5e25e7468ac1563f7d76ca1
Haishan <haishanhan@gmail.com>
2022-04-01 13:38:56 +0800

    Upgrade deps

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,4 @@
 import { defineConfig } from 'vite';
 import { docplugin } from './docplugin';
-// const { defineConfig } = require('vite');
-// const { docplugin } = require('./docplugin');
 
 export default defineConfig(() => ({ plugins: [docplugin()] }));
